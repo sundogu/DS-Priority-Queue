1afb1285b6033753f15f2dc5fa836297fe977dc7
Sunny <sunnbobby@gmail.com>
2019-05-17 17:23:42 +1000

    Update Entry.java

--- src/PriorityQueue/Entry.java
+++ src/PriorityQueue/Entry.java
@@ -2,7 +2,9 @@ package PriorityQueue;
 
 public class Entry<K, V> {
     // Class Variables //
+    /** The key used for comparison */
     private K key;
+    /** The value to store */
     private V value;
 
     // Constructors //
